72ec76f27a61dca7f6a671578fb29832802f214c
Stefan Lukas <etaloof@gmail.com>
2021-02-25 00:46:07 +0100

    Move code around

--- src/drawing/text.rs
+++ src/drawing/text.rs
@@ -9,11 +9,72 @@ use crate::definitions::{Clamp, Image};
 use crate::pixelops::weighted_sum;
 use crate::rect::Rect;
 
+/// Draws colored text on an image in place. `scale` is augmented font scaling on both the x and y axis (in pixels). Note that this function *does not* support newlines, you must do this manually
+pub fn draw_text_mut<'a, I>(
+    image: &'a mut I,
+    color: I::Pixel,
+    x: u32,
+    y: u32,
+    scale: Scale,
+    font: &'a Font<'a>,
+    text: &'a str,
+) where
+    I: GenericImage,
+    <I::Pixel as Pixel>::Subpixel: ValueInto<f32> + Clamp<f32>,
+{
+    let v_metrics = font.v_metrics(scale);
+    let offset = point(0.0, v_metrics.ascent);
+
+    let glyphs: Vec<PositionedGlyph<'_>> = font.layout(text, scale, offset).collect();
+
+    for g in glyphs {
+        if let Some(bb) = g.pixel_bounding_box() {
+            g.draw(|gx, gy, gv| {
+                let gx = gx as i32 + bb.min.x;
+                let gy = gy as i32 + bb.min.y;
+
+                let image_x = gx + x as i32;
+                let image_y = gy + y as i32;
+
+                let image_width = image.width() as i32;
+                let image_height = image.height() as i32;
+
+                if image_x >= 0 && image_x < image_width && image_y >= 0 && image_y < image_height {
+                    let pixel = image.get_pixel(image_x as u32, image_y as u32);
+                    let weighted_color = weighted_sum(pixel, color, 1.0 - gv, gv);
+                    image.put_pixel(image_x as u32, image_y as u32, weighted_color);
+                }
+            })
+        }
+    }
+}
+
+/// Draws colored text on an image in place. `scale` is augmented font scaling on both the x and y axis (in pixels). Note that this function *does not* support newlines, you must do this manually
+pub fn draw_text<'a, I>(
+    image: &'a mut I,
+    color: I::Pixel,
+    x: u32,
+    y: u32,
+    scale: Scale,
+    font: &'a Font<'a>,
+    text: &'a str,
+) -> Image<I::Pixel>
+    where
+        I: GenericImage,
+        <I::Pixel as Pixel>::Subpixel: ValueInto<f32> + Clamp<f32>,
+        I::Pixel: 'static,
+{
+    let mut out = ImageBuffer::new(image.width(), image.height());
+    out.copy_from(image, 0, 0);
+    draw_text_mut(&mut out, color, x, y, scale, font, text);
+    out
+}
+
 /// This helper function is used to find the top (or) left corner of a text.
 /// It takes handles only one dimension per call to make it more reusable.
 /// It takes a `rectangle_size` which is the length (width or height) of the surrounding rectangle
 /// and a `content_size` which is the length (width or height) of the text inside.
-/// The `relative_position` is then used to divide the space 
+/// The `relative_position` is then used to divide the space
 /// which is not used up by the content (=`content_size`) to determine a padding
 /// inside the rectangle `rectangle_size`.
 fn calculate_center(rectangle_size: u32, content_size: u32, relative_position: &EdgePosition) -> u32 {
@@ -377,65 +438,4 @@ impl<'a> GlyphStrings<'a> {
     pub fn width(&self) -> u32 {
         self.0.iter().map(|string| string.width()).sum::<u32>()
     }
-}
-
-/// Draws colored text on an image in place. `scale` is augmented font scaling on both the x and y axis (in pixels). Note that this function *does not* support newlines, you must do this manually
-pub fn draw_text_mut<'a, I>(
-    image: &'a mut I,
-    color: I::Pixel,
-    x: u32,
-    y: u32,
-    scale: Scale,
-    font: &'a Font<'a>,
-    text: &'a str,
-) where
-    I: GenericImage,
-    <I::Pixel as Pixel>::Subpixel: ValueInto<f32> + Clamp<f32>,
-{
-    let v_metrics = font.v_metrics(scale);
-    let offset = point(0.0, v_metrics.ascent);
-
-    let glyphs: Vec<PositionedGlyph<'_>> = font.layout(text, scale, offset).collect();
-
-    for g in glyphs {
-        if let Some(bb) = g.pixel_bounding_box() {
-            g.draw(|gx, gy, gv| {
-                let gx = gx as i32 + bb.min.x;
-                let gy = gy as i32 + bb.min.y;
-
-                let image_x = gx + x as i32;
-                let image_y = gy + y as i32;
-
-                let image_width = image.width() as i32;
-                let image_height = image.height() as i32;
-
-                if image_x >= 0 && image_x < image_width && image_y >= 0 && image_y < image_height {
-                    let pixel = image.get_pixel(image_x as u32, image_y as u32);
-                    let weighted_color = weighted_sum(pixel, color, 1.0 - gv, gv);
-                    image.put_pixel(image_x as u32, image_y as u32, weighted_color);
-                }
-            })
-        }
-    }
-}
-
-/// Draws colored text on an image in place. `scale` is augmented font scaling on both the x and y axis (in pixels). Note that this function *does not* support newlines, you must do this manually
-pub fn draw_text<'a, I>(
-    image: &'a mut I,
-    color: I::Pixel,
-    x: u32,
-    y: u32,
-    scale: Scale,
-    font: &'a Font<'a>,
-    text: &'a str,
-) -> Image<I::Pixel>
-    where
-        I: GenericImage,
-        <I::Pixel as Pixel>::Subpixel: ValueInto<f32> + Clamp<f32>,
-        I::Pixel: 'static,
-{
-    let mut out = ImageBuffer::new(image.width(), image.height());
-    out.copy_from(image, 0, 0);
-    draw_text_mut(&mut out, color, x, y, scale, font, text);
-    out
 }
